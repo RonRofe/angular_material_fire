7c936f665abd74d69d5d6a4627490d6bd80a093f
Ron <ron@rapitec.co.il>
2020-07-11 14:26:50 +0300

    split app components to navigation components

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  @Output() private sideNavToggle: EventEmitter<void> = new EventEmitter<void >();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public onToggleSidenav(): void {
+    this.sideNavToggle.emit();
   }
-
 }
--- src/app/navigation/side-nav-list/side-nav-list.component.ts
+++ src/app/navigation/side-nav-list/side-nav-list.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-side-nav-list',
   templateUrl: './side-nav-list.component.html',
   styleUrls: ['./side-nav-list.component.scss']
 })
-export class SideNavListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class SideNavListComponent {
+  @Output() private closeSidenav: EventEmitter<void> = new EventEmitter<void>();
+  
+  public onClose(): void {
+    this.closeSidenav.emit();
   }
-
 }
