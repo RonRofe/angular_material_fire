a86bb37bc95066723724ba5dc16e35b721a48c9d
Ron <ron@rapitec.co.il>
2020-07-17 17:44:47 +0300

    add subject to update if authenticated and indecate on header

--- src/app/auth/auth-service.service.ts
+++ src/app/auth/auth-service.service.ts
@@ -1,21 +1,29 @@
 import { User } from '../models/user.model';
 import { AuthData } from '../models/auth-data.model';
+import { Subject, Observable } from 'rxjs';
 
 export class AuthService {
+    private authChange: Subject<boolean> = new Subject<boolean>();
     private user: User;
+
+    public getAuthChangeListener(): Observable<boolean> {
+        return this.authChange.asObservable();
+    }
 
     public registerUser(authData: AuthData): void {
          this.user = {
             email: authData.email,
             id: Math.round(Math.random() * 1000000).toString(),
          };
-    }
-
-    public login(authData: AuthData): void {
-        this.user = {
-            email: authData.email,
-            id: Math.round(Math.random() * 1000000).toString(),
-         };   
+         this.authChange.next(true);
+        }
+        
+        public login(authData: AuthData): void {
+            this.user = {
+                email: authData.email,
+                id: Math.round(Math.random() * 1000000).toString(),
+            };   
+            this.authChange.next(true);
     }
 
     public logout(): void {
--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -1,12 +1,26 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+
+import { AuthService } from 'src/app/auth/auth-service.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   @Output() private sideNavToggle: EventEmitter<void> = new EventEmitter<void >();
+
+  public isAuth: boolean;
+
+  constructor(
+    private authService: AuthService,
+  ) {}
+  
+  ngOnInit() {
+    this.authService.getAuthChangeListener().subscribe((status: boolean) => {
+      this.isAuth = status;
+    });
+  }
 
   public onToggleSidenav(): void {
     this.sideNavToggle.emit();
