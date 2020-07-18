011fef7d378d0eaf9978d4dcf517743cceae1612
Ron <ron@rapitec.co.il>
2020-07-18 08:59:12 +0300

    add auth listener to sidenav

--- src/app/navigation/side-nav-list/side-nav-list.component.ts
+++ src/app/navigation/side-nav-list/side-nav-list.component.ts
@@ -1,13 +1,27 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+
+import { AuthService } from 'src/app/auth/auth-service.service';
 
 @Component({
   selector: 'app-side-nav-list',
   templateUrl: './side-nav-list.component.html',
   styleUrls: ['./side-nav-list.component.scss']
 })
-export class SideNavListComponent {
+export class SideNavListComponent implements OnInit {
   @Output() private closeSidenav: EventEmitter<void> = new EventEmitter<void>();
   
+  public isAuth: boolean;
+
+  constructor(
+    private authService: AuthService,
+  ) {}
+
+  ngOnInit() {
+    this.authService.getAuthChangeListener().subscribe((status: boolean) => {
+      this.isAuth = status;
+    });
+  }
+
   public onClose(): void {
     this.closeSidenav.emit();
   }
