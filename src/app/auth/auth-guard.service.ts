49efb388dcbb98f938253fccfb629361a202f167
Ron <ron@rapitec.co.il>
2020-08-10 21:19:10 +0300

    move auth guard to module protection (before module is loading) & set prod environment variables for firebase

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, Router, CanLoad, Route } from '@angular/router';
 
 import { AuthService } from './auth.service';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
     constructor(
         private authService: AuthService,
         private router: Router,
     ) { }
 
     public canActivate(route: ActivatedRouteSnapshot): boolean {
+        return this.authService.isAuth() || (this.router.navigate(['/login']) && false);
+    }
+    
+    public canLoad(route: Route): boolean {
         return this.authService.isAuth() || (this.router.navigate(['/login']) && false);
     }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC2Mpw0oUnKQvptHYIAILGR7RGYgtjlUaY",
+    authDomain: "angularfire-course-dd834.firebaseapp.com",
+    databaseURL: "https://angularfire-course-dd834.firebaseio.com",
+    projectId: "angularfire-course-dd834",
+    storageBucket: "angularfire-course-dd834.appspot.com",
+    messagingSenderId: "441841457071",
+    appId: "1:441841457071:web:e709c963fe3f4cc0a9815a",
+    measurementId: "G-PTV9EC0G04"
+  },
 };
